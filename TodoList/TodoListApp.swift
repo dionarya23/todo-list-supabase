385f4c65f322aeb7dcb8da11c879ef19bf4d9a9d
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-13 20:37:02 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dion Arya Pamungkas on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
